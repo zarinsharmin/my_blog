1481123d9a4b7b5295d16305b3d8683c15a54728
zarinsharmin <zarinshormi97@gmail.com>
2023-09-23 23:55:16 +0600

    first commit

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
